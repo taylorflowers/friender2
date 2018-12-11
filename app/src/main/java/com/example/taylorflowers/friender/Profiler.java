39db0a97715e25ca949f3ecc6a203d39dab9724d
taylorflowers <taylorf3@illinois.edu>
2018-12-10 22:37:05 -0600

    camera

--- app/src/main/java/com/example/taylorflowers/friender/Profiler.java
+++ app/src/main/java/com/example/taylorflowers/friender/Profiler.java
@@ -3,6 +3,7 @@ package com.example.taylorflowers.friender;
 import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.FrameLayout;
 
 public class Profiler extends AppCompatActivity {
@@ -15,10 +16,22 @@ public class Profiler extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profiler);
-
         frameLayout = (FrameLayout)findViewById(R.id.frameLayout);
         camera = Camera.open();
         showCamera = new ShowCamera(this, camera);
         frameLayout.addView(showCamera);
+    }
+
+    Camera.PictureCallback mPictureCallback = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            
+        }
+    };
+
+    public void captureImage(View v) {
+        if (camera != null) {
+            camera.takePicture(null, null, mPictureCallback);
+        }
     }
 }
