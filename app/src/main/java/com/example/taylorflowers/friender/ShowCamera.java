06c1dedf5009e4d938782a6c6574dfb36aaef46d
taylorflowers <taylorf3@illinois.edu>
2018-12-11 21:14:19 -0600

    camera

--- app/src/main/java/com/example/taylorflowers/friender/ShowCamera.java
+++ app/src/main/java/com/example/taylorflowers/friender/ShowCamera.java
@@ -42,11 +42,11 @@ public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
         }
 
         if (this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE) {
-            params.set("orientation","portrait");
+            params.set("orientation","landscape");
             camera.setDisplayOrientation(0);
             params.setRotation(0);
         } else {
-            params.set("orientation","landscape");
+            params.set("orientation","portrait");
             camera.setDisplayOrientation(0);
             params.setRotation(0);
         }
